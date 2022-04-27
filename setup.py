7d0a84819c34ad145eaa584c45d112aef99ef0a2
Eris9 <cool5tarxv@gmail.com>
2022-04-27 09:33:55 +0800

    Hash - fastrack_git

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ import upmindex
 import flags
 import sys
 import index
+import lxml
+import requests
+import lxml.etree
+import lxml._elementpath
 if len(sys.argv) == 1:
     sys.argv.append("py2exe")
-setup(console=['index.py'],options={"py2exe": {"includes": ['upmcore', 'upmguess', 'upmindex',"flags"]}})
+setup(console=['index.py'],options={"py2exe": {"includes": ['upmcore', 'upmguess', 'upmindex',"flags","lxml","requests","lxml._elementpath"]}})
